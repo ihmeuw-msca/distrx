7dc168cdda69c46aae61db2e4b6529c504099f17
zhengp0 <zhengp@uw.edu>
2021-07-01 10:51:19 -0700

    setup package

---
+++ src/distrx/__about__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "distrx"
+__summary__ = "Distribution transformation."
+__uri__ = "https://github.com/ihmeuw-msca/distrx"
+
+__version__ = "0.0.0"
+
+__author__ = "IHME Math Sciences"
+__email__ = "ihme.math.sciences@gmail.com"
+
+__license__ = "BSD 2-Clause License"
+__copyright__ = f"Copyright 2021 {__author__}"
