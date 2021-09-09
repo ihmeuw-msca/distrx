fdcf6a87ca9eb3c1fba5f9a30f977783053af4d2
Kelsey Maass <kmaass@uw.edu>
2021-09-09 16:42:19 -0700

    changed test names

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -6,28 +6,28 @@ import pytest
 from distrx.transforms import get_transform
 
 
-def test_transform_transform_value():
+def test_get_transform_transform_value():
     """Raise ValueError for invalid `transform`."""
     for order in [0, 1, 2]:
         with pytest.raises(ValueError):
             get_transform('dummy', order)
 
 
-def test_transform_order_value():
+def test_get_transform_order_value():
     """Raise ValueError for invalid `order`."""
     for transform in ['log', 'logit', 'exp', 'expit']:
         with pytest.raises(ValueError):
             get_transform(transform, 3)
 
 
-def test_transform_ouput_len():
+def test_get_transform_ouput_len():
     """Length of output should correspond to `order`."""
     for transform in ['log', 'logit', 'exp', 'expit']:
         assert len(get_transform(transform, 1)) == 2
         assert len(get_transform(transform, 2)) == 3
 
 
-def test_transform_output_type():
+def test_get_transform_output_type():
     """Type of output should correspond to `order`."""
     for transform in ['log', 'logit', 'exp', 'expit']:
         assert isinstance(get_transform(transform), types.FunctionType)
