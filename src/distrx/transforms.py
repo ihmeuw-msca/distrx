a2706eb65de85446a33076c32e4fe28f41fad583
Kelsey Maass <kmaass@uw.edu>
2021-09-09 16:37:46 -0700

    write get_transforms

--- src/distrx/transforms.py
+++ src/distrx/transforms.py
@@ -114,17 +114,51 @@ def transform_delta2(mu, sigma, transform):
 def get_transform(transform, order=0):
     """Get transform function and its derivative(s).
 
+    Returns transform function if `order` is 0.
+    Otherwise returns an array of functions, including the transform
+    function and its derivatives up to the specified order.
+
     Parameters
     ----------
-    transform : {'log', 'logit'}
+    transform : {'log', 'logit', 'exp', 'expit'}
         Transform function.
     order : {0, 1, 2}, optional
         Highest order of derivative needed.
 
     Returns
     -------
-    transform : function or array_like of function
+    transforms : function or array_like of function
         Transform function and its derivative(s).
 
     """
-    pass
+    # Check input
+    if transform not in ['log', 'logit', 'exp', 'expit']:
+        raise ValueError(f"Invalid transform function '{transform}'.")
+    if order not in [0, 1, 2]:
+        raise ValueError(f"Invalid order '{order}'.")
+
+    # Define transform functions
+    transform_dict = {
+        'log': [
+            lambda x: np.log,
+            lambda x: 1/x,
+            lambda x: -1/x**2,
+        ], 'logit': [
+            lambda x: np.log(x/(1 - x)),
+            lambda x: 1/(x*(1 - x)),
+            lambda x: (2*x - 1)/(x**2*(1 - x)**2)
+        ], 'exp': [
+            lambda x: np.exp,
+            lambda x: np.exp,
+            lambda x: np.exp
+        ], 'expit': [
+            lambda x: 1/(1 + np.exp(-x)),
+            lambda x: np.exp(-x)/(1 + np.exp(-x))**2,
+            lambda x: np.exp(-x)*(np.exp(-x) - 1)/(1 + np.exp(-x))**3
+        ]
+    }
+
+    # Get function or list of functions
+    if order == 0:
+        return transform_dict[transform][order]
+    return transform_dict[transform][:order+1]
