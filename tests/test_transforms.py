0fcd9994004922871968a3f0aab767a8a48f37d3
Kelsey Maass <kmaass@uw.edu>
2021-09-10 16:15:03 -0700

    add transform_delta2 to function list

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -9,11 +9,11 @@ TODO:
 import numpy as np
 import pytest
 
-from distrx.transforms import transform_data, transform_delta
+from distrx.transforms import transform_data, transform_delta, transform_delta2
 
 
 TRANSFORM_LIST = ['log', 'logit', 'exp', 'expit']
-FUNCTION_LIST = [transform_data, transform_delta]
+FUNCTION_LIST = [transform_data, transform_delta, transform_delta2]
 
 
 @pytest.mark.parametrize("transform", TRANSFORM_LIST)
@@ -21,7 +21,7 @@ def test_method_name_valid(transform):
     """Raise ValueError for invalue `method`."""
     vals = [0.1]*2
     with pytest.raises(ValueError):
-        transform_data(vals, vals, transform, method='dummy') 
+        transform_data(vals, vals, transform, method='dummy')
 
 
 @pytest.mark.parametrize("function", FUNCTION_LIST)
@@ -38,14 +38,14 @@ def test_sigma_positive(function):
     for transform in TRANSFORM_LIST:
         vals = [0.1, -0.1]
         with pytest.raises(ValueError):
-            transform_delta(vals, vals, transform)
+            function(vals, vals, transform)
 
 
 @pytest.mark.parametrize("function", FUNCTION_LIST)
 def test_transform_name_valid(function):
     """Raise ValueError for invalid `transform`."""
     with pytest.raises(ValueError):
-        transform_delta([0.1], [0.1], 'dummy')
+        function([0.1], [0.1], 'dummy')
 
 
 @pytest.mark.parametrize("function", FUNCTION_LIST)
@@ -53,7 +53,7 @@ def test_output_type(function):
     """Output should be numpy arrays."""
     vals = [0.1]*2
     for transform in TRANSFORM_LIST:
-        mu, sigma = transform_delta(vals, vals, transform)
+        mu, sigma = function(vals, vals, transform)
         assert isinstance(mu, np.ndarray)
         assert isinstance(sigma, np.ndarray)
 
@@ -63,5 +63,5 @@ def test_outout_len_match(function):
     """Length of output vectors should match."""
     vals = [0.1]*2
     for transform in TRANSFORM_LIST:
-        mu, sigma = transform_delta(vals, vals, transform)
+        mu, sigma = function(vals, vals, transform)
         assert len(mu) == len(sigma)
